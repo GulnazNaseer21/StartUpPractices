4b3da09000973574996de84077b9155094fbc7da
gulnaznaseer <gnaseerhypersoft@gmail.com>
2023-05-11 21:55:13 -0700

    navigation

---
+++ app/src/main/java/com/hypersoft/myapplication/ViewBalanceFragment.kt
@@ -0,0 +1,10 @@
+package com.hypersoft.myapplication
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ViewBalanceFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ViewBalanceFragment : Fragment(R.layout.fragment_view_balance)
